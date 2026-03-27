dfb38f6c9f8d814762456bc70e824a671a94959e
Sai kumar <saikumaranumalla808@gmail.com>
2026-03-27 18:32:53 +0000

    commit

--- models/example/final_business.sql
+++ models/example/final_business.sql
@@ -1,28 +1,29 @@
-{{config(materialized = 'table', transient ='false')}}
+{{ config(materialized = 'table', transient ='false') }}
 with cust as(
     select
     id as customer_id,
     firstname,
     lastname
-    from{{source('datafeed','raw_customerdata')}}
+    from {{ source('datafeed','raw_customerdata') }}
 ),
 ord as(
     select
     id as order_id,
     user_id as customer_id,
     order_date,
-    status as order_status
-    from{{source('datafeed','raw_orders'))}}
+    status
+    from {{ source('datafeed','raw_orders') }}
 ),
 cust_orders as(
     select
-    min(order_date) as first_order_date
-    max(order_date) as last_order_date
+    customer_id,
+    min(order_date) as first_order_date,
+    max(order_date) as last_order_date,
     count(order_id) as number_of_orders
     from ord
-    group by 1
+    group by customer_id
 ),
-final as(
+final_business as(
     select
     cust.customer_id,
     cust.firstname,
@@ -31,6 +32,7 @@ final as(
     cust_orders.last_order_date,
     coalesce(cust_orders.number_of_orders,0) as num_of_orders
     from cust
-    left join cust_orders using(customer.id)
+    left join cust_orders
+        on cust.customer_id = cust_orders.customer_id
     )
-select * from final
+select * from final_business
